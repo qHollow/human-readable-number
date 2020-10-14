abcc47f2b17c698a1554c587cb9bee23e603ec2e
Reshetnikov Dmitriy <nighthunter2015@gmail.com>
2020-10-14 03:29:35 +0300

    init: start human-readable-number tasks & end him

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
+  const ones = [
+    "",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+  ];
+  const tens = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ];
+  let answ = 0;
+  let dozens = 0;
+  if(number === 0){
+    return 'zero';
+  }
+  if(number < 20){
+    return ones[number];
+  }
+  if(number < 100){
+    answ = Math.floor(number/10);
+    return (tens[answ] + " " + ones[number % 10]).trim();
+  }
+  answ = Math.floor(number / 100);
+  dozens = Math.floor(number % 100);
+  if(dozens >= 20){
+    return (ones[answ] + ' hundred ' + tens[Math.floor(dozens/10)] + " " + ones[dozens % 10]).trim();
+  }
+  else{
+    return (ones[answ] + ' hundred ' + ones[dozens]).trim();
+  }
   
-}
+};
